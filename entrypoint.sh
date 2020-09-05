5d22e06b7b06f584fbf4c673dfa7a987ab8b40be
avvppro <avvppro@gmail.com>
2020-09-05 22:10:05 +0300

    change ' for

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #sed Backend & Frontend ip for Balancing
-sed -i 's BACKEND $BACKEND_IP g' /etc/nginx/conf.d/dtester.conf
-sed -i 's FRONTEND $FRONTEND_IP g' /etc/nginx/conf.d/dtester.conf
+sed -i "s BACKEND $BACKEND_IP g" /etc/nginx/conf.d/dtester.conf
+sed -i "s FRONTEND $FRONTEND_IP g" /etc/nginx/conf.d/dtester.conf
 
 # This will exec the CMD (nginx start) from Dockerfile.
 exec "$@"
